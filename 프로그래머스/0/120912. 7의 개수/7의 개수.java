e6030543abf8dd08d99f62406edeb22ad43c1550
LeeSooBin <102459086+romay99@users.noreply.github.com>
2025-04-07 18:13:55 +0900

    [level 0] Title: 7의 개수, Time: 7.31 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        for(int i = 0 ; i < array.length ; i++){
+            String s = array[i]+"";
+            for(int j = 0 ; j < s.length() ; j++){
+                if(s.charAt(j) == '7') answer++;
+            }
+        }
+        return answer;
+    }
+}
