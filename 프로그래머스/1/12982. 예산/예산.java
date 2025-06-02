5b8e6428980a8e9588781b4487ce9fa79955846c
LeeSooBin <102459086+romay99@users.noreply.github.com>
2025-06-02 16:39:51 +0900

    [level 1] Title: 예산, Time: 0.52 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int answer = 0;
+        
+        for(int i = 0 ; i < d.length ; i++){
+            if(budget < d[i]){
+                break;
+            }
+            answer++;
+            budget -= d[i];
+            
+        }
+        return answer;
+    }
+}
